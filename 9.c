a5a329542fc59799d2b1c2d3bc4c6b13d1f2cbe0
guoj <dev.guoj@gmail.com>
2015-02-16 13:10:03 +0800

    9

---
+++ 9.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+#include "utils.h"
+
+int main(int argc, char const *argv[])
+{
+    for (int a=1; a<500; a++) {
+        for (int b=1; b<500; b++) {
+            for (int c=1; c<500; c++) {
+                if (isPythaNum(a, b, c) && a + b + c == 1000) {
+                    printf("%d %d %d\n", a, b, c);
+                } 
+            }
+        }
+    }
+
+    return 0;
+}
